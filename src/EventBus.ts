8f19059b9aaab6afa4e0f14dccd2df33ef4000e1
Ryan Smythe <ryan.smythe@gmail.com>
2018-02-06 09:55:36 -0500

    Added comments. Renamed a bunch of stuff for clarity. Got rid of event constructor hashing. Using a regEx to get the event type name instead.

--- src/EventBus.ts
+++ src/EventBus.ts
@@ -1,66 +1,131 @@
 namespace EventBusTS
 {
-    export class EventListener<TListener, TEvent extends EventBase>
+    // EventSubscription
+    // An object/method pair that takes in a specific event type
+    //  Used to ensure the method is performed on the correct scope ('this')
+    //
+    // TTarget: The type of object that is listening
+    // TEvent: The type of event to listen for
+    //
+    export class EventSubscription<TTarget, TEvent extends EventBase>
     {
-        public constructor (public scope: TListener, public callback: (event: TEvent) => void)
-        {
-
-        }
+        // target: An instantiated object that is handling this type of event
+        // handler: The method that will handle the event
+        //
+        public constructor (public target: TTarget, public handler: (event: TEvent) => void)
+        { }
     }
 
     export class EventBus
     {
-        public static listeners: { [key: string]: EventListener<any, EventBase>[] } = {};
+        // eventRegistry
+        //
+        // eventRegistry: Contains a property for each event type that can be handled
+        //  Each event type has a list of EventSubscriptions subscribed to it
+        //
+        private static eventRegistry: { [key: string]: EventSubscription<any, EventBase>[] } = {};
 
-        public static addEventListener<TEvent extends EventBase, TListenerScope>(event: IEventConstructor, callback: (event: TEvent) => void, scope: TListenerScope): void
+        // register
+        //
+        // Adds the event type to 'eventRegistry' if it doesn't already exist
+        //  Registers target.handler() as a listener for eventType
+        //
+        // TEvent: The type of event to listen for
+        // TTarget: The type of object that is listening
+        //
+        // eventType: The event type (constructor)
+        // handler: The method that will handle the event
+        // target: The object to apply the handler method against
+        //
+        public static register<TEvent extends EventBase, TTarget>(eventType: IEventConstructor, handler: (evt: TEvent) => void, target: TTarget): void
         {
-            let typeHash: string = this.hash(event.toString());
-            let listener: EventListener<TListenerScope, TEvent> = new EventListener(scope, callback);
+            let eventTypeName: string = this.getEventName(eventType);
+            let listener: EventSubscription<TTarget, TEvent> = new EventSubscription(target, handler);
 
-            this.listeners.hasOwnProperty(typeHash) ? this.listeners[typeHash].push(listener) : this.listeners[typeHash] = [listener];
+            this.eventRegistry.hasOwnProperty(eventTypeName) ? this.eventRegistry[eventTypeName].push(listener) : this.eventRegistry[eventTypeName] = [listener];
         }
 
+        // unregister
+        //
+        // Removes any EventSubscriptions for 'eventType' registered by 'target'
+        //
+        // eventType: Event type 
+        // target: The object that is no longer handling this type of event
+        //
+        public static unregister<TTarget>(eventType: IEventConstructor, target: TTarget): void
+        {
+            let eventTypeName: string = this.getEventName(eventType);
+            if(this.eventRegistry.hasOwnProperty(eventTypeName) )
+            {
+                let eventRegistration: EventSubscription<any, EventBase>[] = this.eventRegistry[eventTypeName];
+                eventRegistration
+                    .filter(l => l.target !== target)
+                    .forEach((listener, idx) =>
+                    {
+                        eventRegistration.splice(eventRegistration.indexOf(listener), 1);
+                    });
+            }
+        }
+
+        // dispatch
+        //
+        // Finds the event type in the event registry and calls all listeners
+        //  The event object is passed as the first parameter
+        // 
+        // TEvent: The type of event being raised
+        //
+        // event: The raised event
+        //
         public static dispatch<TEvent extends EventBase>(event: TEvent): void
         {
-            let typeHash: string = this.hash(event.constructor.toString());
-            if(this.listeners.hasOwnProperty(typeHash) )
+            let eventConstructor: IEventConstructor = <any>event.constructor;
+            let eventType: string = this.getEventName(eventConstructor);
+            if(this.eventRegistry.hasOwnProperty(eventType) )
             {
-                let listeners: EventListener<any, TEvent>[] = this.listeners[typeHash];
+                let listeners: EventSubscription<any, TEvent>[] = this.eventRegistry[eventType];
                 listeners.forEach((listener) =>
                 {
-                    if(listener && listener.callback) 
+                    if(listener && listener.handler) 
                     {
-                        listener.callback.apply(listener.scope, [event]);
+                        listener.handler.apply(listener.target, [event]);
                     }
                 });
             }
         }
 
-        private static hash(str: string): string
+        // getEventName
+        //
+        // Used to get the type of an  (there is no simple way to do this in ES5)
+        //  In ES6 this can be replaced with 'eventType.name'
+        //
+        // https://stackoverflow.com/questions/2648293/how-to-get-the-function-name-from-within-that-function
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/name
+        //
+        private static getEventName(eventType: IEventConstructor): string
         {
-            let hash: number = 5381;
-            let i: number = str.length;
-
-            while(i) {
-                hash = (hash * 33) ^ str.charCodeAt(--i);
-            }
-
-            /* JavaScript does bitwise operations (like XOR, above) on 32-bit signed
-            * integers. Since we want the results to be always positive, convert the
-            * signed int to an unsigned by doing an unsigned bitshift. */
-            let hashed: number = hash >>> 0;
-
-            return hashed.toString(16);
+            let result: RegExpExecArray = /^function\s+([\w\$]+)\s*\(/.exec( eventType.toString() );
+            return result  ?  result[ 1 ]  :  '';
         }
     }
 
+    // IEventConstructor
+    //
+    // Just shorthand for a constructor that returns any EventBase type
+    //
     export interface IEventConstructor
     {
         new (...args: any[]): EventBase;
     }
 
+
+    // EventBase
+    //
+    // Base class to denote an event type
+    //  Technically we could allow any object to be dispatched as an event
+    //
     export abstract class EventBase
     {
+        // Might want to remove this. It feels weird telling an event to dispatch itself.
         public Dispatch(): void
         {
             EventBus.dispatch(this);
--- src/HandlesDecorator.ts
+++ src/HandlesDecorator.ts
@@ -1,47 +1,63 @@
 ﻿/* tslint:disable:no-unused-variable */
 namespace EventBusTS
 {
+    // IHandlerConstructor
+    // Represents the constructor for a type decorated with StateObserver
+    //
+    // T: The type of object returned by the constructor
     export interface IHandlerConstructor<T> extends Function
     {
+        // A dictionary of [MethodName: { EventType }]
         __handlers__?: {
-            [prop: string]: { Event: IEventConstructor }
+            [prop: string]: IEventConstructor
         };
+
+        // Standard constructor
         new (...args: any[]): T;
     }
 
+
     export function Handles(event: IEventConstructor): MethodDecorator
     {
-        let f: MethodDecorator = function (target: any, propertyKey: string, descriptor: TypedPropertyDescriptor<any>): any
+        let f: MethodDecorator = function (target: any, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<any>): any
         {
             const targetType: IHandlerConstructor<typeof target.constructor> = target.constructor;
             targetType.__handlers__ = targetType.__handlers__ || {};
-            targetType.__handlers__[propertyKey] = { Event: event };
+            targetType.__handlers__[propertyKey] = event;
         };
         return f;
     }
 
+    // Wraps a constructor with our event bus wireup logic
     export function StateObserver<T>(target: IHandlerConstructor<T>): any
     {
         let originalConstructor: IHandlerConstructor<T> = target;
 
-        //Create a new function to replace the constructor
+        // Create a new function to replace the constructor
         let newConstructor: Function = function (...args: any[]): T
         {
-            //Construct the object
+            // Construct the object
             let constructedObject: T = new originalConstructor(...args);
 
+            // Check if our custom property exists on the constructor
             if (originalConstructor.__handlers__)
             {
+                // Loop through all decorated handler methods registered on this type
                 for (let prop in originalConstructor.__handlers__)
                 {
-                    if (constructedObject[prop])
+                    // Double-check that a method with that name exists on this object
+                    if (prop in constructedObject)
                     {
-                        let eventHandler: (evt: EventBusTS.EventBase) => void = (<{ [prop: string]: (evt: EventBusTS.EventBase) => void }><any>constructedObject )[prop];
+                        // Get the event handler method from the constructed object
+                        let eventHandler: (evt: EventBusTS.EventBase) => void = (<any>constructedObject)[prop];
                         eventHandler.bind(constructedObject);
 
-                        let event: IEventConstructor = originalConstructor.__handlers__[prop].Event;
+                        // Event type to handle is stored in __handlers__[MethodName]
+                        // ie: originalConstructor.__handlers__['OnDateChanged'] -> DateChangedEvent
+                        let eventType: IEventConstructor = originalConstructor.__handlers__[prop];
  
-                        EventBus.addEventListener(event, eventHandler, constructedObject);
+                        // Register this object (constructedObject) to handle this event type (eventType) by calling this method (eventHandler)
+                        EventBus.register(eventType, eventHandler, constructedObject);
                     }
                 }
             }
@@ -53,6 +69,6 @@ namespace EventBusTS
 
         //return the new function as the object constructor
         return newConstructor;
-    }
-    /* tslint:enable:no-unused-variable */
+    }    
 }
+/* tslint:enable:no-unused-variable */
